064c24ec4731f23133fcd7d83229555398312f64
Murilo <142051797+murilosantosb@users.noreply.github.com>
2024-07-26 19:40:40 -0300

    Começando a fazer o front-end

--- frontend/react-food/src/app/layout.tsx
+++ frontend/react-food/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppis = Poppins({
+  subsets: ["latin"],
+  weight: ["400","600"]
+}, )
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={poppis.className}>{children}</body>
     </html>
   );
 }
